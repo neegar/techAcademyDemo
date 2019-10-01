2d7fea15abccea33117121d0a4304571f5562af8
neegar <nigarahmdva@gmail.com>
2019-10-01 14:26:13 +0400

    my first demo

---
+++ assets/js/teachers.js
@@ -0,0 +1,5 @@
+function showMyImage() {
+    var img = document.getElementById('image-holder');
+    img.src = "http://anthemprep.greatheartsacademies.org/wp-content/uploads/sites/12/2016/12/default-placeholder.png";
+    img.style.display = 'block';
+  }
